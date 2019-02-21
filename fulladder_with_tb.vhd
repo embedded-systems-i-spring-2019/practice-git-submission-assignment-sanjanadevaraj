d017af63030fd325bd3033bf305071a52a0e48a7
sanjanadevaraj <47343318+sanjanadevaraj@users.noreply.github.com>
2019-02-20 22:53:21 -0500

    Full adder with test bench

--- fulladder_with_tb.vhd
+++ fulladder_with_tb.vhd
@@ -29,7 +29,7 @@ entity Testbench_structural_adder is
 end Testbench_structural_adder;
  
 architecture  behavioral of Testbench_structural_adder is
- component Full_Adder_Structural_VHDL 
+ component Full_Adder_Structural_VHDL
    port( 
   X1, X2, Cin : in std_logic;  
   S, Cout : out std_logic
